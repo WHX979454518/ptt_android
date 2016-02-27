c8753ab537cf7346580fae803c2c16c92f000431
fanchao <simophin@gmail.com>
2016-02-27 23:00:19 +1300

    整理资源帮助类

--- app/src/main/kotlin/com/xianzhitech/ptt/ext/Resources.kt
+++ app/src/main/kotlin/com/xianzhitech/ptt/ext/Resources.kt
@@ -1,16 +1,14 @@
 package com.xianzhitech.ptt.ext
 
 import android.content.Context
-import android.os.Build
 import android.support.annotation.ColorInt
 import android.support.annotation.ColorRes
 import android.support.annotation.DrawableRes
+import android.support.v4.content.ContextCompat
 import android.support.v4.graphics.drawable.DrawableCompat
 import com.xianzhitech.ptt.R
 import com.xianzhitech.ptt.repo.RoomWithMemberNames
 import com.xianzhitech.ptt.repo.RoomWithMembers
-import kotlin.collections.joinToString
-import kotlin.text.isNullOrBlank
 
 /**
  *
@@ -19,23 +17,19 @@ import kotlin.text.isNullOrBlank
  * Created by fanchao on 25/12/15.
  */
 
-fun Context.getDrawableCompat(@DrawableRes drawableRes: Int) =
-        if (Build.VERSION.SDK_INT >= 21) getDrawable(drawableRes)
-        else resources.getDrawable(drawableRes)
-
 fun Context.getTintedDrawable(@DrawableRes drawableRes: Int, @ColorInt tintColor: Int) =
         DrawableCompat.wrap(getDrawableCompat(drawableRes)).apply {
             DrawableCompat.setTint(this, tintColor)
         }
 
-fun Context.getColorCompat(@ColorRes colorRes: Int) =
-        if (Build.VERSION.SDK_INT >= 23) getColor(colorRes)
-        else resources.getColor(colorRes)
+fun Context.getColorCompat(@ColorRes colorRes: Int) = ContextCompat.getColor(this, colorRes)
+fun Context.getDrawableCompat(@DrawableRes drawableRes: Int) = ContextCompat.getDrawable(this, drawableRes)
 
 fun Int.toColorValue(context: Context) = context.getColorCompat(this)
 fun Int.toDimen(context: Context) = context.resources.getDimension(this)
-
 fun Int.toFormattedString(context: Context, vararg args: Any?) = context.getString(this, *args)
+
+//private fun List<String>.members
 
 fun RoomWithMemberNames.getMemberNames(context: Context) =
         context.resources.getString(if (memberCount > memberNames.size) R.string.group_member_with_more else R.string.group_member,
