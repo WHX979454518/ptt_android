dba3163adbce923c7ded03e483429d387c5cdc49
fanchao <simophin@gmail.com>
2016-05-01 13:12:29 +1200

    修改错误提示以及改善修改密码界面

--- app/src/main/kotlin/com/xianzhitech/ptt/ui/user/ChangePasswordActivity.kt
+++ app/src/main/kotlin/com/xianzhitech/ptt/ui/user/ChangePasswordActivity.kt
@@ -1,157 +1,34 @@
 package com.xianzhitech.ptt.ui.user
 
-import android.app.ProgressDialog
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.TextInputLayout
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import com.xianzhitech.ptt.AppComponent
 import com.xianzhitech.ptt.R
-import com.xianzhitech.ptt.ext.*
-import com.xianzhitech.ptt.service.describeInHumanMessage
 import com.xianzhitech.ptt.ui.base.BaseToolbarActivity
-import com.xianzhitech.ptt.ui.dialog.AlertDialogFragment
 
 
-class ChangePasswordActivity : BaseToolbarActivity(), AlertDialogFragment.OnPositiveButtonClickListener, AlertDialogFragment.OnNegativeButtonClickListener {
-    private lateinit var oldPassword : TextInputLayout
-    private lateinit var newPassword : TextInputLayout
-    private lateinit var newPasswordConfirmed : TextInputLayout
-
-    private class EditTextAutoClearErrorWatcher(private val editText: TextInputLayout) : TextWatcher {
-        override fun afterTextChanged(s: Editable?) {
-            editText.error = null
-        }
-
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
-    }
+class ChangePasswordActivity : BaseToolbarActivity(), ChangePasswordFragment.Callbacks {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_change_password)
-
-        oldPassword = findView(R.id.changePassword_oldPassword)
-        newPassword = findView(R.id.changePassword_newPassword)
-        newPasswordConfirmed = findView(R.id.changePassword_newPasswordConfirm)
-
-        oldPassword.editText?.addTextChangedListener(EditTextAutoClearErrorWatcher(oldPassword))
-        newPassword.editText?.addTextChangedListener(EditTextAutoClearErrorWatcher(newPassword))
-        newPasswordConfirmed.editText?.addTextChangedListener(EditTextAutoClearErrorWatcher(newPasswordConfirmed))
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        super.onCreateOptionsMenu(menu)
-        menuInflater.inflate(R.menu.change_password, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.changePassword_save) {
-            savePassword()
-            return true
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.content, ChangePasswordFragment(), TAG_FRAGMENT)
+                .commit()
         }
-        return super.onOptionsItemSelected(item)
     }
 
-
-    override fun finish() {
-        if (oldPassword.visibility == View.VISIBLE && oldPassword.text.isNullOrEmpty().not() ||
-                (newPassword.text.isNullOrEmpty().not() || newPasswordConfirmed.text.isNullOrEmpty().not())) {
-            AlertDialogFragment.Builder().apply {
-                title = R.string.dialog_confirm_title.toFormattedString(this@ChangePasswordActivity)
-                message = R.string.error_has_unsaved_change.toFormattedString(this@ChangePasswordActivity)
-                btnPositive = R.string.quit.toFormattedString(this@ChangePasswordActivity)
-                btnNegative = R.string.dialog_cancel.toFormattedString(this@ChangePasswordActivity)
-            }.show(supportFragmentManager, TAG_CONFIRM_QUIT)
-
-            return
-        }
-
+    override fun confirmFinishing() {
         super.finish()
     }
 
-    override fun onPositiveButtonClicked(fragment: AlertDialogFragment) {
-        when (fragment.tag) {
-            TAG_CONFIRM_QUIT -> super.finish()
-            else -> super.onPositiveButtonClicked(fragment)
+    override fun finish() {
+        if ((supportFragmentManager.findFragmentByTag(TAG_FRAGMENT) as? ChangePasswordFragment)?.requestFinish() ?: true) {
+            super.finish()
         }
     }
-
-    override fun onNegativeButtonClicked(fragment: AlertDialogFragment) {
-        when (fragment.tag) {
-            TAG_CONFIRM_QUIT -> fragment.dismiss()
-            else -> super.onNegativeButtonClicked(fragment)
-        }
-    }
-
-    private fun savePassword() {
-        if ((oldPassword.editText!!.isEmpty())) {
-            oldPassword.error = R.string.error_input_password.toFormattedString(this)
-            oldPassword.requestFocusFromTouch()
-            return
-        }
-
-        if (newPassword.editText!!.isEmpty()) {
-            newPassword.error = R.string.error_input_password.toFormattedString(this)
-            newPassword.requestFocusFromTouch()
-            return
-        }
-
-        if (newPasswordConfirmed.editText!!.isEmpty()) {
-            newPasswordConfirmed.error = R.string.error_input_password.toFormattedString(this)
-            newPasswordConfirmed.requestFocusFromTouch()
-            return
-        }
-
-        if (newPasswordConfirmed.text != newPassword.text) {
-            val error = R.string.error_password_not_same.toFormattedString(this)
-            newPasswordConfirmed.error = error
-            newPassword.error = error
-            newPassword.requestFocusFromTouch()
-            return
-        }
-
-        val password = newPassword.text
-        if (password == null || password.length < 6) {
-            val error = R.string.error_password_not_long_enough.toFormattedString(this)
-            newPasswordConfirmed.error = error
-            newPassword.error = error
-            return
-        }
-
-        oldPassword.error = null
-        newPassword.error = null
-        newPasswordConfirmed.error = null
-
-        val dialog = ProgressDialog.show(this, null, R.string.saving.toFormattedString(this), true, false)
-        (application as AppComponent).signalService
-                .changePassword(oldPassword.text!!, password)
-                .observeOnMainThread()
-                .doOnError {
-                    if (!isFinishing) {
-                        Snackbar.make(newPassword, it.describeInHumanMessage(this), Snackbar.LENGTH_LONG).show()
-                    }
-
-                    dialog.dismiss()
-                }
-                .subscribeSimple {
-                    dialog.dismiss()
-                    if (!isFinishing) {
-                        super.finish()
-                    }
-                }
-    }
-
-    private val TextInputLayout.text : String?
-        get() = editText?.text?.toString()
 
     companion object {
-        private const val TAG_CONFIRM_QUIT = "tag_confirm_quit"
+        private const val TAG_FRAGMENT = "tag_frag"
     }
 }
---
+++ app/src/main/kotlin/com/xianzhitech/ptt/ui/user/ChangePasswordFragment.kt
@@ -0,0 +1,158 @@
+package com.xianzhitech.ptt.ui.user
+
+import android.app.ProgressDialog
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.TextInputLayout
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.xianzhitech.ptt.AppComponent
+import com.xianzhitech.ptt.R
+import com.xianzhitech.ptt.ext.*
+import com.xianzhitech.ptt.service.describeInHumanMessage
+import com.xianzhitech.ptt.ui.base.BaseFragment
+import com.xianzhitech.ptt.ui.dialog.AlertDialogFragment
+
+class ChangePasswordFragment : BaseFragment(), AlertDialogFragment.OnPositiveButtonClickListener, AlertDialogFragment.OnNegativeButtonClickListener {
+    private var views : Views? = null
+    private val callbacks : Callbacks?
+    get() = (activity as? Callbacks?)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_change_password, container, false).apply {
+            views = Views(this).apply {
+                oldPassword.editText?.addTextChangedListener(EditTextAutoClearErrorWatcher(oldPassword))
+                newPassword.editText?.addTextChangedListener(EditTextAutoClearErrorWatcher(newPassword))
+                newPasswordConfirm.editText?.addTextChangedListener(EditTextAutoClearErrorWatcher(newPasswordConfirm))
+                saveButton.setOnClickListener { savePassword() }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        views = null
+        super.onDestroyView()
+    }
+    
+    fun requestFinish() : Boolean {
+        views?.apply {
+            if (oldPassword.visibility == View.VISIBLE && oldPassword.text.isNullOrEmpty().not() ||
+                    (newPassword.text.isNullOrEmpty().not() || newPasswordConfirm.text.isNullOrEmpty().not())) {
+                AlertDialogFragment.Builder().apply {
+                    title = R.string.dialog_confirm_title.toFormattedString(context)
+                    message = R.string.error_has_unsaved_change.toFormattedString(context)
+                    btnPositive = R.string.quit.toFormattedString(context)
+                    btnNegative = R.string.dialog_cancel.toFormattedString(context)
+
+                }.show(childFragmentManager, TAG_CONFIRM_QUIT)
+
+                return false
+            }
+        }
+
+        return true
+    }
+
+    private fun savePassword() {
+        views?.apply {
+            if ((oldPassword.editText!!.isEmpty())) {
+                oldPassword.error = R.string.error_input_password.toFormattedString(context)
+                oldPassword.requestFocusFromTouch()
+                return
+            }
+
+            if (newPassword.editText!!.isEmpty()) {
+                newPassword.error = R.string.error_input_password.toFormattedString(context)
+                newPassword.requestFocusFromTouch()
+                return
+            }
+
+            if (newPasswordConfirm.editText!!.isEmpty()) {
+                newPasswordConfirm.error = R.string.error_input_password.toFormattedString(context)
+                newPasswordConfirm.requestFocusFromTouch()
+                return
+            }
+
+            if (newPasswordConfirm.text != newPassword.text) {
+                val error = R.string.error_password_not_same.toFormattedString(context)
+                newPasswordConfirm.error = error
+                newPassword.error = error
+                newPassword.requestFocusFromTouch()
+                return
+            }
+
+            val password = newPassword.text
+            if (password == null || password.length < 6) {
+                val error = R.string.error_password_not_long_enough.toFormattedString(context)
+                newPasswordConfirm.error = error
+                newPassword.error = error
+                return
+            }
+
+            oldPassword.error = null
+            newPassword.error = null
+            newPasswordConfirm.error = null
+
+            val dialog = ProgressDialog.show(context, null, R.string.saving.toFormattedString(context), true, false)
+            (activity.application as AppComponent).signalService
+                    .changePassword(oldPassword.text!!, password)
+                    .observeOnMainThread()
+                    .doOnError {
+                        if (activity != null) {
+                            Toast.makeText(context, it.describeInHumanMessage(context), Snackbar.LENGTH_LONG).show()
+                        }
+
+                        dialog.dismiss()
+                    }
+                    .subscribeSimple {
+                        dialog.dismiss()
+                        if (activity != null && !activity.isFinishing) {
+                            callbacks?.confirmFinishing()
+                        }
+                    }
+        }
+    }
+
+    override fun onPositiveButtonClicked(fragment: AlertDialogFragment) {
+        when (fragment.tag) {
+            TAG_CONFIRM_QUIT -> callbacks?.confirmFinishing()
+        }
+    }
+
+    override fun onNegativeButtonClicked(fragment: AlertDialogFragment) {
+        when (fragment.tag) {
+            TAG_CONFIRM_QUIT -> fragment.dismiss()
+        }
+    }
+
+
+    private val TextInputLayout.text : String?
+        get() = editText?.text?.toString()
+
+    private class EditTextAutoClearErrorWatcher(private val editText: TextInputLayout) : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            editText.error = null
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+    }
+
+    private class Views(rootView : View,
+                        val oldPassword : TextInputLayout = rootView.findView(R.id.changePassword_oldPassword),
+                        val newPassword : TextInputLayout = rootView.findView(R.id.changePassword_newPassword),
+                        val newPasswordConfirm : TextInputLayout = rootView.findView(R.id.changePassword_newPasswordConfirm),
+                        val saveButton : View = rootView.findView(R.id.changePassword_save))
+
+    interface Callbacks {
+        fun confirmFinishing()
+    }
+
+    companion object {
+        private const val TAG_CONFIRM_QUIT = "tag_confirm_quit"
+    }
+}
