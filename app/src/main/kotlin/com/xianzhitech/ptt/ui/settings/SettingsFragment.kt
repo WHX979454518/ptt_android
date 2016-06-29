0126f4c3794a4c59ec00bd6e8bd3874acfa98a00
fanchao <simophin@gmail.com>
2016-06-29 21:32:56 +1200

    注释掉快捷按键设置

--- app/src/main/kotlin/com/xianzhitech/ptt/ui/settings/SettingsFragment.kt
+++ app/src/main/kotlin/com/xianzhitech/ptt/ui/settings/SettingsFragment.kt
@@ -1,17 +1,13 @@
 package com.xianzhitech.ptt.ui.settings
 
 import android.os.Bundle
-import android.support.v7.preference.ListPreference
-import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
 import com.xianzhitech.ptt.R
 import com.xianzhitech.ptt.Shortcut
 import com.xianzhitech.ptt.ShortcutMode
 import com.xianzhitech.ptt.ext.appComponent
-import com.xianzhitech.ptt.ext.subscribeSimple
 import com.xianzhitech.ptt.ext.toFormattedString
 import rx.Single
-import rx.android.schedulers.AndroidSchedulers
 import rx.subscriptions.CompositeSubscription
 
 class SettingsFragment : PreferenceFragmentCompat () {
@@ -19,7 +15,7 @@ class SettingsFragment : PreferenceFragmentCompat () {
     private lateinit var modeFromContact : CharSequence
     private lateinit var modeFromRoom : CharSequence
 
-    private lateinit var shortcutPref: ListPreference
+//    private lateinit var shortcutPref: ListPreference
     private var shortcutSubscription : CompositeSubscription? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,22 +30,22 @@ class SettingsFragment : PreferenceFragmentCompat () {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences)
 
-        shortcutPref = findPreference(getString(R.string.key_shortcut_mode)) as ListPreference
-        shortcutPref.entries = arrayOf(modeNoOp, modeFromContact, modeFromRoom)
-        shortcutPref.entryValues = shortcutPref.entries
-        shortcutPref.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, any ->
-            when (any) {
-                modeNoOp -> {
-                    val shortcut = Shortcut(ShortcutMode.NO_OP)
-                    appComponent.preference.shortcut = shortcut
-                    applyShortcut(shortcut)
-                }
-                modeFromContact -> {
-
-                }
-            }
-            false
-        }
+//        shortcutPref = findPreference(getString(R.string.key_shortcut_mode)) as ListPreference
+//        shortcutPref.entries = arrayOf(modeNoOp, modeFromContact, modeFromRoom)
+//        shortcutPref.entryValues = shortcutPref.entries
+//        shortcutPref.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, any ->
+//            when (any) {
+//                modeNoOp -> {
+//                    val shortcut = Shortcut(ShortcutMode.NO_OP)
+//                    appComponent.preference.shortcut = shortcut
+//                    applyShortcut(shortcut)
+//                }
+//                modeFromContact -> {
+//
+//                }
+//            }
+//            false
+//        }
     }
 
     override fun onStart() {
@@ -67,17 +63,17 @@ class SettingsFragment : PreferenceFragmentCompat () {
     }
 
     private fun applyShortcut(shortcut: Shortcut) {
-        shortcutSubscription?.apply {
-            add(shortcut.calleeName
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeSimple {
-                    if (it == null) {
-                        shortcutPref.summary = R.string.shortcut_mode_no_op.toFormattedString(activity)
-                    } else {
-                        shortcutPref.summary = R.string.shortcut_with_callee.toFormattedString(activity, it)
-                    }
-                })
-        }
+//        shortcutSubscription?.apply {
+//            add(shortcut.calleeName
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribeSimple {
+//                    if (it == null) {
+//                        shortcutPref.summary = R.string.shortcut_mode_no_op.toFormattedString(activity)
+//                    } else {
+//                        shortcutPref.summary = R.string.shortcut_with_callee.toFormattedString(activity, it)
+//                    }
+//                })
+//        }
     }
 
     private val Shortcut.calleeName : Single<CharSequence?>
