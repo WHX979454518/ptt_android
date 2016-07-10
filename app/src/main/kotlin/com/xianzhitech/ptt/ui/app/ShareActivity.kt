773f44d8caf2bce03f583320964c85caac933994
Fanchao Liu <fanchao@Fanchaos-MacBook-Pro.local>
2016-07-10 12:36:44 +1200

    恢复下载地址

--- app/src/main/kotlin/com/xianzhitech/ptt/ui/app/ShareActivity.kt
+++ app/src/main/kotlin/com/xianzhitech/ptt/ui/app/ShareActivity.kt
@@ -6,9 +6,10 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
-import com.xianzhitech.ptt.Constants
 import com.xianzhitech.ptt.R
-import com.xianzhitech.ptt.ext.*
+import com.xianzhitech.ptt.ext.appComponent
+import com.xianzhitech.ptt.ext.findView
+import com.xianzhitech.ptt.ext.toFormattedString
 import com.xianzhitech.ptt.ui.base.BaseToolbarActivity
 
 
@@ -23,30 +24,21 @@ class ShareActivity : BaseToolbarActivity() {
 
         barcodeView = findView(R.id.share_barCode)
         shareButton = findViewById(R.id.share_btn)
-    }
 
-    override fun onStart() {
-        super.onStart()
+        val downloadUrl = "${appComponent.appServerEndpoint}/app/latest"
+        Glide.with(this)
+                .load(Uri.parse("http://qr.liantu.com/api.php?&bg=ffffff&fg=000000")
+                        .buildUpon()
+                        .appendQueryParameter("w", barcodeView.layoutParams.width.toString())
+                        .appendQueryParameter("text", downloadUrl)
+                        .build())
+                .into(barcodeView)
 
-        appComponent.appService.retrieveAppConfig(appComponent.signalHandler.currentUserId ?: Constants.EMPTY_USER_ID)
-                .toObservable()
-                .observeOnMainThread()
-                .compose(bindToLifecycle())
-                .subscribeSimple { appConfig ->
-                    Glide.with(this)
-                            .load(Uri.parse("http://qr.liantu.com/api.php?&bg=ffffff&fg=000000")
-                                    .buildUpon()
-                                    .appendQueryParameter("w", barcodeView.layoutParams.width.toString())
-                                    .appendQueryParameter("text", appConfig.downloadUrl)
-                                    .build())
-                            .into(barcodeView)
-
-                    shareButton.setOnClickListener {
-                        startActivity(Intent.createChooser(
-                                Intent(Intent.ACTION_SEND).setType("text/plain").putExtra(Intent.EXTRA_TEXT, R.string.share_text.toFormattedString(this, appConfig.downloadUrl)),
-                                R.string.share_via.toFormattedString(this)
-                        ))
-                    }
-                }
+        shareButton.setOnClickListener {
+            startActivity(Intent.createChooser(
+                    Intent(Intent.ACTION_SEND).setType("text/plain").putExtra(Intent.EXTRA_TEXT, R.string.share_text.toFormattedString(this, downloadUrl)),
+                    R.string.share_via.toFormattedString(this)
+            ))
+        }
     }
 }
