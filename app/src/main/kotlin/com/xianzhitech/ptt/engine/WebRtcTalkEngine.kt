88703ba89f6b71dc0ed8a55597955871c768dfcd
fanchao <simophin@gmail.com>
2016-12-25 11:25:40 +1300

    回退引擎

--- app/src/main/kotlin/com/xianzhitech/ptt/engine/WebRtcTalkEngine.kt
+++ app/src/main/kotlin/com/xianzhitech/ptt/engine/WebRtcTalkEngine.kt
@@ -3,19 +3,17 @@ package com.xianzhitech.ptt.engine
 import android.content.Context
 import android.os.Handler
 import android.os.HandlerThread
-import cn.netptt.engine.VoiceEngine
-import com.xianzhitech.ptt.ext.e
-import com.xianzhitech.ptt.ext.i
-import com.xianzhitech.ptt.ext.print
 import com.xianzhitech.ptt.ext.subscribeSimple
 import com.xianzhitech.ptt.service.VoiceService
 import com.xianzhitech.ptt.service.VoiceServiceJoinRoomRequest
 import okhttp3.OkHttpClient
-import org.slf4j.LoggerFactory
+import org.webrtc.autoim.MediaEngine
+import org.webrtc.autoim.NativeWebRtcContextRegistry
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.net.InetAddress
+import java.util.concurrent.atomic.AtomicBoolean
 
 /**
 
@@ -29,16 +27,19 @@ class WebRtcTalkEngine(context: Context,
     private val handler: Handler
     private val context: Context
 
-    private lateinit var mediaEngine: VoiceEngine
-    private var mediaChannel : Int? = null
+    private lateinit var mediaEngine: MediaEngine
     private lateinit var voiceService : VoiceService
     private var extProtoData: IntArray? = null
-    var highQualityMode : Boolean = false
-    private set
 
     init {
         this.handler = Handler(ENGINE_THREAD.looper)
         this.context = context.applicationContext
+
+        if (hasRegisteredWebRtc.compareAndSet(false, true)) {
+            handler.post {
+                NativeWebRtcContextRegistry().apply { register(this@WebRtcTalkEngine.context) }
+            }
+        }
     }
 
     fun connect(roomId: String, property: Map<String, Any?>) {
@@ -50,35 +51,17 @@ class WebRtcTalkEngine(context: Context,
         this.extProtoData = intArrayOf((roomIdLong ushr 32).toInt(), (roomIdLong and 0xFFFFFFFF).toInt())
 
         handler.post {
-            mediaEngine = VoiceEngine(context)
-            logger.i { "Available codecs: ${mediaEngine.availableCodecs.print()}" }
-
-            mediaChannel = mediaEngine.createChannel(property[PROPERTY_PROTOCOL] == "tcp")
-
-            applySoundQuality()
-
-            logger.i { "Current codec set to: ${mediaEngine.getSendCodec(mediaChannel!!)}" }
-
+            mediaEngine = MediaEngine(context, property[PROPERTY_PROTOCOL]?.equals("tcp") ?: false)
             val userId = property[PROPERTY_LOCAL_USER_ID]?.toString()?.toInt()
-            if (!mediaEngine.setLocalSSRC(mediaChannel!!, userId ?: throw IllegalArgumentException("User id is null"))) {
-                logger.e { "Error setting local ssrc: ${mediaEngine.lastErr}" }
-            }
-            if (!mediaEngine.setSendDestination(mediaChannel!!,
-                    property[PROPERTY_REMOTE_SERVER_ADDRESS]?.toString()?.resolveToIPAddress() ?: throw IllegalArgumentException("No server ip specified"),
-                    property[PROPERTY_REMOTE_SERVER_PORT]?.toString()?.toInt() ?: throw IllegalArgumentException("No report port specified"))) {
-                logger.e { "Error setting dest ssrc: ${mediaEngine.lastErr}" }
-            }
-            if (!mediaEngine.setLocalReceiver(mediaChannel!!, LOCAL_RTP_PORT, LOCAL_RTCP_PORT)) {
-                logger.e { "Error setting local local receiver: ${mediaEngine.lastErr}" }
-            }
-
-            if (!mediaEngine.startListen(mediaChannel!!)) {
-                logger.e { "Error start listening: ${mediaEngine.lastErr}" }
-            }
-
-            if (!mediaEngine.startPlayout(mediaChannel!!)) {
-                logger.e { "Error start playing out: ${mediaEngine.lastErr}" }
-            }
+            mediaEngine.setLocalSSRC(userId ?: throw IllegalArgumentException("User id is null"))
+            mediaEngine.setRemoteIp(property[PROPERTY_REMOTE_SERVER_ADDRESS]?.toString()?.resolveToIPAddress() ?: throw IllegalArgumentException("No server ip specified"))
+            mediaEngine.setAudioTxPort(property[PROPERTY_REMOTE_SERVER_PORT]?.toString()?.toInt() ?: throw IllegalArgumentException("No report port specified"))
+            mediaEngine.setAudioRxPort(LOCAL_RTP_PORT, LOCAL_RTCP_PORT)
+            mediaEngine.setAgc(true)
+            mediaEngine.setNs(true)
+            mediaEngine.setEc(true)
+            mediaEngine.setAudio(true)
+            mediaEngine.start()
 
             voiceService = Retrofit.Builder()
                     .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
@@ -90,63 +73,37 @@ class WebRtcTalkEngine(context: Context,
 
             voiceService.command(VoiceServiceJoinRoomRequest(roomId, userId.toString(), userId.toLong())).subscribeSimple()
 
-            if (!mediaEngine.sendExtPacket(mediaChannel!!, RTP_EXT_PROTO_JOIN_ROOM, extProtoData)) {
-                logger.e { "Error sending join packet: ${mediaEngine.lastErr}" }
-            }
-
+            mediaEngine.sendExtPacket(RTP_EXT_PROTO_JOIN_ROOM, extProtoData)
             try {
                 Thread.sleep(100)
             } catch(e: Exception) {
             }
-
-            if (!mediaEngine.sendExtPacket(mediaChannel!!, RTP_EXT_PROTO_JOIN_ROOM, extProtoData)) {
-                logger.e { "Error sending join packet: ${mediaEngine.lastErr}" }
-            }
+            mediaEngine.sendExtPacket(RTP_EXT_PROTO_JOIN_ROOM, extProtoData)
 
             scheduleHeartbeat()
-        }
-    }
-
-    private fun applySoundQuality() {
-        if (!mediaEngine.setSendCodec(mediaChannel!!, if (highQualityMode) HIGH_QUALITY_CODEC else LOW_QUALITY_CODEC)) {
-            logger.e { "Error setting codec: ${mediaEngine.lastErr}" }
         }
     }
 
     private fun scheduleHeartbeat() {
         handler.postDelayed({
-            mediaEngine.sendExtPacket(mediaChannel!!, RTP_EXT_PROTO_HEARTBEAT, extProtoData)
+            mediaEngine.sendExtPacket(RTP_EXT_PROTO_HEARTBEAT, extProtoData)
             scheduleHeartbeat()
         }, HEARTBEAT_INTERVAL_MILLS)
     }
 
-    fun enableHighQualityMode(enable : Boolean) {
-        if (highQualityMode != enable) {
-            highQualityMode = enable
-
-            handler.post {
-                if (mediaChannel != null) {
-                    applySoundQuality()
-                }
-            }
-        }
-    }
-
     fun startSend() {
-        handler.post { mediaEngine.startSend(mediaChannel!!) }
+        handler.post { mediaEngine.startSend() }
     }
 
     fun stopSend() {
-        handler.post { mediaEngine.stopSend(mediaChannel!!) }
+        handler.post { mediaEngine.stopSend() }
     }
 
     fun dispose() {
         handler.post {
-            mediaEngine.sendExtPacket(mediaChannel!!, RTP_EXT_PROTO_QUIT_ROOM, extProtoData)
-            mediaEngine.stopSend(mediaChannel!!)
-            mediaEngine.stopListen(mediaChannel!!)
-            mediaEngine.destroy()
-            mediaChannel = null
+            mediaEngine.sendExtPacket(RTP_EXT_PROTO_QUIT_ROOM, extProtoData)
+            mediaEngine.stop()
+            mediaEngine.dispose()
             handler.removeCallbacksAndMessages(null)
         }
     }
@@ -169,10 +126,7 @@ class WebRtcTalkEngine(context: Context,
         private val LOCAL_RTP_PORT = 0
         private val LOCAL_RTCP_PORT = 0
 
-        private val HIGH_QUALITY_CODEC = VoiceEngine.CodecInst("opus", 64000, 960, 2, 48000)
-        private val LOW_QUALITY_CODEC = VoiceEngine.CodecInst("opus", 8000, 960, 1, 48000)
-
-        private val logger = LoggerFactory.getLogger("TalkEngine")
         private val ENGINE_THREAD = HandlerThread("RTCEngineThread").apply { start() }
+        private val hasRegisteredWebRtc = AtomicBoolean(false)
     }
 }
