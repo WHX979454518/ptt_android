d6650f543e63d907633374b3ed315e80efd84788
fanchao <simophin@gmail.com>
2017-04-15 23:38:28 +1200

    节约图标资源

--- app/src/main/kotlin/com/xianzhitech/ptt/ui/widget/ModelView.kt
+++ app/src/main/kotlin/com/xianzhitech/ptt/ui/widget/ModelView.kt
@@ -16,6 +16,7 @@ import com.xianzhitech.ptt.ui.widget.drawable.MultiDrawable
 import com.xianzhitech.ptt.ui.widget.drawable.TextDrawable
 import com.xianzhitech.ptt.ui.widget.drawable.UriDrawable
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 
@@ -40,50 +41,40 @@ class ModelView @JvmOverloads constructor(context: Context,
     private fun subscribeIfNeeded() {
         val model = this.model
         if (isAttached && model != null) {
-            val drawable: Observable<Drawable>
+            val drawable: Single<Drawable>
 
             when (model) {
-                is User -> drawable = getUserDrawable(model)
+                is User -> {
+                    setImageDrawable(createUserDrawable(model))
+                    return
+                }
                 is ContactGroup -> {
                     drawable = context.appComponent.storage.getUsers(model.memberIds.toList().atMost(9))
-                            .switchMap {
-                                Observable.combineLatest(it.map(this::getUserDrawable)) {
-                                    @Suppress("UNCHECKED_CAST")
-                                    MultiDrawable(context, it.toList() as List<Drawable>)
-                                }
-                            }
+                            .firstOrError()
+                            .map { MultiDrawable(context, it.map(this::createUserDrawable)) }
                 }
                 is Room -> {
                     drawable = context.appComponent.storage.getRoomMembers(model, 9)
-                            .switchMap {
-                                Observable.combineLatest(it.map(this::getUserDrawable)) {
-                                    @Suppress("UNCHECKED_CAST")
-                                    MultiDrawable(context, it.toList() as List<Drawable>)
-                                }
-                            }
+                            .firstOrError()
+                            .map { MultiDrawable(context, it.map(this::createUserDrawable)) }
                 }
                 else -> throw IllegalStateException("Unknown model type $model")
             }
 
-            disposable = drawable.subscribe(this::setImageDrawable)
+            disposable = drawable
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(this::setImageDrawable)
         }
     }
 
-    private fun getUserDrawable(model: User): Observable<Drawable> {
-        return context.appComponent.storage.getUsers(listOf(model.id))
-                .flatMap { it.firstOrNull()?.let { Observable.just(it as User) } ?: Observable.empty() }
-                .observeOn(AndroidSchedulers.mainThread())
-                .startWith(model)
-                .distinctUntilChanged()
-                .map {
-                    if (model.avatar.isNullOrEmpty()) {
-                        TextDrawable(model.name[0].toString(), resources.getIntArray(R.array.account_colors).let {
-                            it[Math.abs(model.name.hashCode()) % it.size]
-                        })
-                    } else {
-                        UriDrawable(Glide.with(context), Uri.parse(model.avatar))
-                    }
-                }
+    private fun createUserDrawable(model: User): Drawable {
+        return if (model.avatar.isNullOrEmpty()) {
+            TextDrawable(model.name[0].toString(), resources.getIntArray(R.array.account_colors).let {
+                it[Math.abs(model.name.hashCode()) % it.size]
+            })
+        } else {
+            UriDrawable(Glide.with(context), Uri.parse(model.avatar))
+        }
     }
 
     override fun onAttachedToWindow() {
