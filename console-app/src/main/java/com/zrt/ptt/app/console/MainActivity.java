89c402ad8574c296dbe898af10196886bedb7d27
Dorsey Chen <642296308@qq.com>
2017-04-20 09:05:23 +0800

    console-appUI界面

--- console-app/src/main/java/com/zrt/ptt/app/console/MainActivity.java
+++ console-app/src/main/java/com/zrt/ptt/app/console/MainActivity.java
@@ -3,12 +3,39 @@ package com.zrt.ptt.app.console;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.baidu.mapapi.map.MapView;
 
 public class MainActivity extends AppCompatActivity {
-
+   private MapView mMapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//remove title bar 即隐藏标题栏
+        getSupportActionBar().hide();// 隐藏ActionBar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//remove notification bar 即全屏
         setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.bmapView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
     }
 }
