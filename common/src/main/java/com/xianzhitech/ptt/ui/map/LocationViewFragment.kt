871ed47d4143418221db0652740ba2f74ad73c81
fanchao <simophin@gmail.com>
2017-04-30 19:34:27 +1200

    增加位置显示的图标

--- common/src/main/java/com/xianzhitech/ptt/ui/map/LocationViewFragment.kt
+++ common/src/main/java/com/xianzhitech/ptt/ui/map/LocationViewFragment.kt
@@ -1,14 +1,22 @@
 package com.xianzhitech.ptt.ui.map
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import android.location.Location
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v4.graphics.drawable.DrawableCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.baidu.mapapi.map.BitmapDescriptorFactory
+import com.baidu.mapapi.map.MapStatusUpdateFactory
 import com.baidu.mapapi.map.MapView
-import com.baidu.mapapi.map.MyLocationData
+import com.baidu.mapapi.map.MarkerOptions
 import com.baidu.mapapi.model.LatLng
 import com.baidu.mapapi.utils.CoordinateConverter
+import com.xianzhitech.ptt.R
 import com.xianzhitech.ptt.ui.base.BaseFragment
 
 
@@ -28,7 +36,23 @@ class LocationViewFragment : BaseFragment() {
             it.convert()
         }
 
-        mapView.map.setMyLocationData(MyLocationData.Builder().latitude(position.latitude).longitude(position.longitude).accuracy(loc.accuracy).build())
+        mapView.map.animateMapStatus(MapStatusUpdateFactory.newLatLng(position))
+
+        val drawable = DrawableCompat.wrap(ContextCompat.getDrawable(context, R.drawable.ic_location_on_black_30dp))
+        DrawableCompat.setTint(drawable, ContextCompat.getColor(context, R.color.material_red300))
+
+        val bitmap = Bitmap.createBitmap((drawable.intrinsicWidth * 1.5).toInt(), (drawable.intrinsicHeight * 1.5).toInt(), Bitmap.Config.ARGB_8888).apply {
+            Canvas(this).let {
+                drawable.setBounds(0, 0, width, height)
+                drawable.draw(it)
+            }
+        }
+
+        mapView.map.addOverlay(MarkerOptions()
+                .position(position)
+                .anchor(0.5f, 1f)
+                .icon(BitmapDescriptorFactory.fromBitmap(bitmap))
+        )
         return mapView
     }
 
