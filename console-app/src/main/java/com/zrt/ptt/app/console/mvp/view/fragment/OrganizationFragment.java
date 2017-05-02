beeaf9c4942da03a538a408a74a5703cd54c38b3
Dorsey Chen <642296308@qq.com>
2017-05-02 10:32:10 +0800

    console-app获取组织架构RXjava没能执行拿不到用户数据

--- console-app/src/main/java/com/zrt/ptt/app/console/mvp/view/fragment/OrganizationFragment.java
+++ console-app/src/main/java/com/zrt/ptt/app/console/mvp/view/fragment/OrganizationFragment.java
@@ -36,7 +36,9 @@ import java.util.LinkedList;
 import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 
@@ -67,133 +69,81 @@ public class OrganizationFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_organization, container, false);
         treeLv = (ListView) view.findViewById(R.id.tree_lv);
 
-
-
-       /* contactEnterprise.observeOn(AndroidSchedulers.mainThread()).subscribe(enterprise->{
-
-           List<ContactDepartment> contactDepartment = enterprise.getDepartments();
-           OrgNodeBean org ;
-            Log.e("contactDepartment:------->",""+contactDepartment);
-            for (ContactDepartment department :contactDepartment){
-               org = new OrgNodeBean(department.getId(),department.getParentObjectId(),department.getName());
-                Log.e(""+department.getName()+":",""+department.getName());
-               mDatas.add(org);
-
-           }
-           List<ContactUser> contactUser = enterprise.getDirectUsers();
-            Log.e("contactUser:------->",""+contactUser);
-            for (ContactUser user:contactUser){
-               org = new OrgNodeBean(user.getId(),user.getParentObjectId(),user.getName());
-                mDatas.add(org);
-           }
-       });*/
-//                 = contactEnterprise.observeOn(AndroidSchedulers.mainThread())    .subscribe(enterprise -> {        // do something with enterprise    })
-
-//        initDatas();
-
-
         AppComponent appComponent = (AppComponent) App.getInstance().getApplicationContext();
-
         Observable<ContactEnterprise> contactEnterprise = appComponent.getSignalBroker().getEnterprise();
-        /*Observable<String> nomalObservable = Observable.create(new Observable.OnSubscribe<String>(){
-
-        });
-//        contactEnterprise.observeOn(AndroidSchedulers.mainThread()).subscribe(new Actio);
-        contactEnterprise
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread()
-                )
-                .subscribe(new Subscriber<List<ContactEnterprise>>() {
-
-
-                    @Override
-                    public void onSubscribe(Subscription subscription) {
-
-                    }
-
-                    @Override
-                    public void onNext(List<ContactEnterprise> contactEnterprises) {
-
-                    }
-
-                    @Override
-                    public void onError(Throwable throwable) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-
-
-                });*/
-
-        contactEnterprise.observeOn(AndroidSchedulers.mainThread()).subscribe(contactEnterprise1 -> {
-            List<ContactDepartment> contactDepartment = contactEnterprise1.getDepartments();
-            OrgNodeBean org;
-
-            Log.e("contactDepartment:------->", "" + contactDepartment);
-            for (ContactDepartment department : contactDepartment) {
-                org = new OrgNodeBean(department.getId(), department.getParentObjectId(), department.getName());
-                Log.e("" + department.getName() + ":", "" + department.getName());
-                mDatas.add(org);
+        contactEnterprise.observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<ContactEnterprise>() {
+            @Override
+            public void onSubscribe(Disposable disposable) {
 
             }
-            List<ContactUser> contactUser = contactEnterprise1.getDirectUsers();
-            Log.e("contactUser:------->", "" + contactUser);
-            for (ContactUser user : contactUser) {
-                org = new OrgNodeBean(user.getId(), user.getParentObjectId(), user.getName());
-                mDatas.add(org);
-            }
 
-            try {
-                adapter = new MyTreeListViewAdapter<OrgNodeBean>(treeLv, getActivity(),
-                        mDatas, 0, isHide);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
+            @Override
+            public void onNext(ContactEnterprise contactEnterprise) {
+                List<ContactDepartment> contactDepartment = contactEnterprise.getDepartments();
+                OrgNodeBean org ;
+                Log.e("contactDepartment","contactDepartment:"+contactDepartment);
+                for (ContactDepartment department :contactDepartment){
+                    org = new OrgNodeBean(department.getId(),department.getParentObjectId(),department.getName());
+                    Log.e(""+department.getName()+":",""+department.getName());
+                    mDatas.add(org);
 
-            adapter.setOnTreeNodeClickListener(new TreeListViewAdapter.OnTreeNodeClickListener() {
-                @Override
-                public void onClick(Node node, int position) {
-                    if (node.isLeaf()) {
-                        Toast.makeText(getActivity().getApplicationContext(), node.getName(),
-                                Toast.LENGTH_SHORT).show();
-                    }
                 }
 
-                @Override
-                public void onCheckChange(Node node, int position,
-                                          List<Node> checkedNodes) {
-                    // TODO Auto-generated method stub
-
-                    StringBuffer sb = new StringBuffer();
-                    for (Node n : checkedNodes) {
-                        /*int pos = n.get_Id() - 1;
-                        sb.append(mDatas.get(pos).getName()).append("---")
-                                .append(pos + 1).append(";");*/
-
-                    }
-
-                    /*Toast.makeText(getActivity().getApplicationContext(), sb.toString(),
-                            Toast.LENGTH_SHORT).show();*/
+                List<ContactUser> contactUser = contactEnterprise.getDirectUsers();
+                Log.e("contactUser:------->",""+contactUser);
+                for (ContactUser user:contactUser){
+                    org = new OrgNodeBean(user.getId(),user.getParentObjectId(),user.getName());
+                    mDatas.add(org);
                 }
 
-            });
+                try {
+                    adapter = new MyTreeListViewAdapter<OrgNodeBean>(treeLv, getActivity(),
+                            mDatas, 0, isHide);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
 
-            treeLv.setAdapter(adapter);
+                adapter.setOnTreeNodeClickListener(new TreeListViewAdapter.OnTreeNodeClickListener() {
+                    @Override
+                    public void onClick(Node node, int position) {
+                        if (node.isLeaf()) {
+                            Toast.makeText(getActivity().getApplicationContext(), node.getName(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
 
+                    @Override
+                    public void onCheckChange(Node node, int position,
+                                              List<Node> checkedNodes) {
+                        // TODO Auto-generated method stub
 
+                        StringBuffer sb = new StringBuffer();
+                        for (Node n : checkedNodes) {
+
+                        }
+                    }
+
+                });
+
+                treeLv.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
         });
 
         return view;
 
     }
 
-    interface  Action{
-         void call();
-    }
 
     public void initDatas() {
         String json = "{\"status\":200,\"data\":[{\"lastLocationTimeCST\":\"\",\"positionname\":\"总经理\",\"locateicon\":\"http://netptt.cn:19999/img/group1/M00/00/00/eSkWC1jcogiAYgUMAABsrdgKnKM450.png_24_24\",\"icon\":\"http://netptt.cn:19999/img/group1/M00/00/00/eSkWC1jbb1SAS_t4AACAR6D-mGo442.png_16_16\",\"_id\":\"58fec69ff3ca2d0007beb0e7\",\"commander\":1,\"password\":\"670b14728ad9902aecba32e22fa4f6bd\",\"enterprise\":\"58fec69ff3ca2d0007beb0e6\",\"phoneNbr\":11122233344,\"idNumber\":9100001,\"name\":\"指挥号\",\"__v\":0,\"position\":\"59014861f3ca2d0007beb151\",\"father\":\"58fec8a1f3ca2d0007beb0eb\",\"mail\":\"\",\"privileges\":{\"priority\":null,\"viewMap\":false,\"muteAble\":true,\"powerInviteAble\":true,\"calledOuterAble\":true,\"callOuterAble\":true,\"forbidSpeak\":true,\"joinAble\":true,\"calledAble\":true,\"groupAble\":true,\"callAble\":true}},{\"lastLocationTimeCST\":\"\",\"positionname\":\"组长\",\"locateicon\":\"http://netptt.cn:19999/img/group1/M00/00/00/eSkWC1jco4GAAfhgAABrtMxgZOY283.png_24_24\",\"icon\":\"http://netptt.cn:19999/img/group1/M00/00/00/eSkWC1jbb1SAS_t4AACAR6D-mGo442.png_16_16\",\"_id\":\"58fec91df3ca2d0007beb0ec\",\"enterprise\":\"58fec69ff3ca2d0007beb0e6\",\"name\":\"五杀\",\"father\":\"58fec8a1f3ca2d0007beb0eb\",\"password\":\"670b14728ad9902aecba32e22fa4f6bd\",\"mail\":\"asd@dom.com\",\"idNumber\":9100002,\"phoneNbr\":13500000000,\"__v\":0,\"position\":\"59014876f3ca2d0007beb152\",\"privileges\":{\"priority\":3,\"viewMap\":true,\"muteAble\":true,\"powerInviteAble\":true,\"calledOuterAble\":true,\"callOuterAble\":true,\"forbidSpeak\":true,\"joinAble\":true,\"calledAble\":true,\"groupAble\":true,\"callAble\":true}},{\"lastLocationTimeCST\":\"\",\"positionname\":\"组长\",\"locateicon\":\"http://netptt.cn:19999/img/group1/M00/00/00/eSkWC1jco4GAAfhgAABrtMxgZOY283.png_24_24\",\"icon\":\"http://netptt.cn:19999/img/group1/M00/00/00/eSkWC1jbb1SAS_t4AACAR6D-mGo442.png_16_16\",\"_id\":\"5901491ff3ca2d0007beb156\",\"enterprise\":\"58fec69ff3ca2d0007beb0e6\",\"name\":\"流浪汉\",\"father\":\"59014806f3ca2d0007beb150\",\"position\":\"59014876f3ca2d0007beb152\",\"password\":\"670b14728ad9902aecba32e22fa4f6bd\",\"mail\":\"aaad@dom.com\",\"idNumber\":9100003,\"phoneNbr\":13566666666,\"__v\":0,\"privileges\":{\"priority\":3,\"viewMap\":true,\"muteAble\":true,\"powerInviteAble\":true,\"calledOuterAble\":true,\"callOuterAble\":true,\"forbidSpeak\":true,\"joinAble\":true,\"calledAble\":true,\"groupAble\":true,\"callAble\":true}},{\"lastLocationTimeCST\":\"\",\"positionname\":\"平民\",\"locateicon\":\"http://netptt.cn:19999/img/group1/M00/00/00/eSkWC1jcoqaAXNIPAABq9HszdDo314.png_24_24\",\"icon\":\"http://netptt.cn:19999/img/group1/M00/00/00/eSkWC1jbb1SAS_t4AACAR6D-mGo442.png_16_16\",\"_id\":\"5901495af3ca2d0007beb157\",\"enterprise\":\"58fec69ff3ca2d0007beb0e6\",\"name\":\"打手\",\"father\":\"59014806f3ca2d0007beb150\",\"position\":\"590148a2f3ca2d0007beb154\",\"password\":\"670b14728ad9902aecba32e22fa4f6bd\",\"mail\":\"adf@dom.com\",\"idNumber\":9100004,\"phoneNbr\":13522222222,\"__v\":0,\"privileges\":{\"priority\":3,\"viewMap\":true,\"muteAble\":true,\"powerInviteAble\":true,\"calledOuterAble\":true,\"callOuterAble\":true,\"forbidSpeak\":true,\"joinAble\":true,\"calledAble\":true,\"groupAble\":true,\"callAble\":true}},{\"_id\":\"58fec8a1f3ca2d0007beb0eb\",\"enterprise\":\"58fec69ff3ca2d0007beb0e6\",\"isParent\":true,\"father\":\"58fec69ff3ca2d0007beb0e6\",\"name\":\"青铜\",\"__v\":0},{\"_id\":\"59014806f3ca2d0007beb150\",\"enterprise\":\"58fec69ff3ca2d0007beb0e6\",\"isParent\":true,\"father\":\"58fec69ff3ca2d0007beb0e6\",\"name\":\"撸撸撸\",\"__v\":0},{\"father\":-1,\"name\":\"czqtest\",\"_id\":\"58fec69ff3ca2d0007beb0e6\",\"isParent\":true}]}";
